3a828db7920991d158226c5484bd1dc64a9d2737
Ulzahk <ulzahk@gmail.com>
2020-09-25 22:30:29 -0500

    [UPDATED] favorite-rooms test

--- src/tests/favorite.rooms.routes.test.js
+++ src/tests/favorite.rooms.routes.test.js
@@ -1,10 +1,9 @@
 const proxyquire = require('proxyquire')
-const assert = require('assert')
-const FavoriteRoomsControllerMock = require('../utils/mocks/favoriteRooms')
+const { FavoriteRoomsControllerMock } = require('../utils/mocks/favoriteRooms')
 const testServer = require('../utils/testServer')
 
 describe('Favorite Rooms - Route', () => {
-    let router = proxyquire('../components/favorite-rooms/routes', {
+    const router = proxyquire('../components/favorite-rooms/routes', {
         './controller': FavoriteRoomsControllerMock
     })
 
@@ -12,20 +11,20 @@ describe('Favorite Rooms - Route', () => {
 
     describe('POST /api/favorite-rooms/add', () => {
         it('Should respond with status 200', (done) => {
-            request.post('/add').expect(200, done())
+            request.post('/api/favorite-rooms/add').expect(200, done)
         })
 
         it('Should respond with a Content-type json', (done) => {
-            request.post('/add').expect('Content-type', /json/, done())
+            request.post('/api/favorite-rooms/add').expect('Content-type', /json/, done)
         })
     })
 
     describe('POST /api/favorite-rooms/remove', () => {
         it('Should respond with status 200', (done) => {
-            request.post('/remove').expect(200, done())
+            request.post('/api/favorite-rooms/remove').expect(200, done)
         })
         it('Should respond with a Content-type json', (done) => {
-            request.post('/remove').expect('Content-type', /json/, done())
+            request.post('/api/favorite-rooms/remove').expect('Content-type', /json/, done)
         })
     })
 })
